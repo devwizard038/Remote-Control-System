0b0382f89bc2effa3a79d82d012adfe20283c051
WizardDev <129702384+wizarddev038@users.noreply.github.com>
2023-05-09 18:26:27 +0900

    Delete translate directory

--- translate/translate-node14.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-%LOCALAPPDATA%\..\Roaming\nvm\v14.16.0\node translate.js minifyall
-%LOCALAPPDATA%\..\Roaming\nvm\v14.16.0\node translate.js translateall
-%LOCALAPPDATA%\..\Roaming\nvm\v14.16.0\node translate.js extractall
--- translate/translate.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-node translate.js minifyall
-node translate.js translateall
-node translate.js extractall
-pause
